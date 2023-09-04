3fd5f45b66edef695d2079a7b3997e98e449d2b2
Yunxe <784312513@qq.com>
2023-09-04 16:10:37 +0800

    test ac host

--- cmd/gpt/gpt.go
+++ cmd/gpt/gpt.go
@@ -16,6 +16,7 @@ func main() {
 	// 假设第二个参数是 cURL 响应体
 	if len(args) >= 4 {
 		curlResponse := args[1]
+		fmt.Println(args[3])
 		os.Setenv("AZURE_OPENAI_APIKEY", args[2])
 		os.Setenv("AZURE_OPENAI_HOST", args[3])
 		gpt.NewGPT(curlResponse)
