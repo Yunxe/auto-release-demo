c69e2d1ab402b8af5b7122f9f16529719229712a
Yunxe <784312513@qq.com>
2023-09-04 16:21:09 +0800

    test fix

--- cmd/gpt/gpt.go
+++ cmd/gpt/gpt.go
@@ -9,10 +9,6 @@ import (
 func main() {
 	args := os.Args
 
-	// 打印所有参数
-	fmt.Println("Number of arguments:", len(args))
-	fmt.Println("Arguments:", args)
-
 	// 假设第二个参数是 cURL 响应体
 	if len(args) >= 4 {
 		curlResponse := args[1]
